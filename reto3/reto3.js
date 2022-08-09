167b91a00d7725655f0ed4c1954438c54d14fe78
Evelio <eveliosanchez@spaplaneamento.com>
2022-08-09 07:48:02 +0200

    Reto3 solved

---
+++ reto3/reto3.js
@@ -0,0 +1,4 @@
+function ocultar_numeros(numeros, char_hide = 'x'){
+    const oculto =  numeros.toString().replace(/\s/g, '')
+    return(char_hide.repeat(13) + oculto.toString().slice(oculto.toString().length-3,oculto.toString().length))
+}
