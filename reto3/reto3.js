892b7caa52c94f6fc707089d336416de8efefc85
Evelio <eveliosanchez@spaplaneamento.com>
2022-08-09 08:04:11 +0200

    Añadida comprobación de longitud

--- reto3/reto3.js
+++ reto3/reto3.js
@@ -1,4 +1,4 @@
 function ocultar_numeros(numeros, char_hide = 'x'){
     const oculto =  numeros.toString().replace(/\s/g, '')
-    return(char_hide.repeat(13) + oculto.toString().slice(oculto.toString().length-3,oculto.toString().length))
+    return(oculto.length != 16 ? false : char_hide.repeat(13) + oculto.toString().slice(oculto.toString().length-3,oculto.toString().length))
 }
